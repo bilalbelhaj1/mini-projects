95b814fae3074cc493015b4717f001142beca4d6
bilal belhaj <bilalbelhadj2025@gmail.com>
2025-12-17 14:56:27 +0100

    query the user saved movies formappwrite database

---
+++ project1/src/api/getSavedMovies.js
@@ -0,0 +1,21 @@
+import { tablesDB } from "../services/appwriteClient";
+import { Query } from "appwrite";
+
+const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
+const TBALE_ID = import.meta.env.VITE_TABLE_ID;
+
+export const getSavedMovies = async (userId) => {
+    try {
+        const data = await tablesDB.listRows({
+            databaseId:DATABASE_ID,
+            tableId:TBALE_ID,
+            queries: [
+                Query.equal('userId', userId),
+            ]
+        })
+        return data;
+    } catch(err) {
+        console.log(err)
+        throw err;
+    }
+}
--- project1/src/routes/savedmovies.lazy.jsx
+++ project1/src/routes/savedmovies.lazy.jsx
@@ -1,10 +1,22 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { requireAuth } from '../services/requireAuth'
+import { useContext } from 'react'
+import { UserContext } from '../contexts/userContext'
+import { useQuery } from '@tanstack/react-query'
+import { getSavedMovies } from '../api/getSavedMovies'
 export const Route = createLazyFileRoute('/savedmovies')({
   component: SavedMovies,
 })
 
 function SavedMovies() {
   requireAuth()
+  const { user } = useContext(UserContext);
+  const { isLoading, data } = useQuery({
+    queryKey:[user],
+    queryFn: ()=>getSavedMovies(user.userId),
+    staleTime: 3000
+  })
+  console.log(data);
+  if(isLoading) return <h2 style={{paddingTop: "200px"}}>Loading.....</h2>
   return <div style={{background:"red", height:"900px"}} >saved movies goes here</div>
 }
